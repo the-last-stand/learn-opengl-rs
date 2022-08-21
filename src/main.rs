a552e059ec6053927e6d749ce0beb916dea18031
KoShin <koshin.1116@gmail.com>
2022-08-21 17:28:42 +0800

    feat: 1_1_1 hello window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// #[cfg(features = "chapter-1")]
+use crate::_1_getting_started::main_1_1_1;
+// #[cfg(features = "chapter-1")]
+mod _1_getting_started;
+
 fn main() {
-    println!("Hello, world!");
+    main_1_1_1();
 }
